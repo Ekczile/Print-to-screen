9bc00f797a58211528632208b55a6df2585f8d1e
Steven <ekczile@gmail.com>
2018-09-19 16:27:37 -0400

    reworked Print

--- Print.rb
+++ Print.rb
@@ -1,9 +1,15 @@
-100.times do |x|
-	if x == 2
- 	puts "minded"
- 	elsif x == 4
- 		puts "minds"
- 	else 
- 		puts x + 1
-end
+for x in 1..100
+
+	if x % 15 == 0
+      puts "Minded Minds"
+
+   elsif x % 5 == 0 
+      puts "Minds"
+
+   elsif x % 3 == 0
+      puts "Mined"
+
+   else
+      puts x
+   end
 end
