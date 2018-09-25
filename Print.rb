925e51764b70865336abfa61df8af79c8ffaad3b
Steven <ekczile@gmail.com>
2018-09-25 19:56:19 -0400

    faulure tests

--- Print.rb
+++ Print.rb
@@ -1,15 +1,18 @@
-for x in 1..100
+def minedminds()
 
-	if x % 15 == 0
+      for x in 1..100
+
+	      if x % 15 == 0
       puts "Minded Minds"
 
-   elsif x % 5 == 0 
+             elsif x % 5 == 0 
       puts "Minds"
 
-   elsif x % 3 == 0
+             elsif x % 3 == 0
       puts "Mined"
 
-   else
+             else
       puts x
-   end
+             end
+      end
 end
