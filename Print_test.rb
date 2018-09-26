d01b785abfe4cf90327fb6b1857b105aec00bd7a
Steven <ekczile@gmail.com>
2018-09-25 20:08:08 -0400

    fail 2

--- Print_test.rb
+++ Print_test.rb
@@ -6,4 +6,9 @@ class PrintTest < Minitest::Test
     def test_Mined
         assert_equal("Mined",minedminds(3))
     end
+
+    def test_minds
+        assert_equal("Minds",minedminds(5))
+    end
+
 end
