da7362d8f168bddb4a8a1186012869ec9395ebb2
Steven <ekczile@gmail.com>
2018-09-25 20:00:34 -0400

    faulure tests

--- Print_test.rb
+++ Print_test.rb
@@ -6,12 +6,4 @@ class PrintTest < Minitest::Test
     def test_Mined
         assert_equal("Mined",minedminds(3))
     end
-
-    def test_Minds
-        assert_equal("Minds",minedminds(5))
-    end
-
-    def test_Mindedminds
-        assert_equal("MinededMinds",minedminds(15))
-    end
 end
